da8c593f71efef449ae8354d2e545acd92566b98
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-25 06:23:35 -0200

    Removing 'engines' prop on package.json

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send({ test: 'new folder structure'})
-})
+// Authentication Lib
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+passport.use(new GoogleStrategy())
 
 app.listen(PORT)
