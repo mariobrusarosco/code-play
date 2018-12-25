e236a3f64edece8ef8c5f84c9d2af27897ba1646
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-25 06:01:59 -0200

    Changing Folder structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send({ test: 'testing'})
+    res.send({ test: 'new folder structure'})
 })
 
 app.listen(PORT)
