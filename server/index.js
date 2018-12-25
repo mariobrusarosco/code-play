97116647f87e23a0a22d6755d3d6111971f87538
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-25 14:26:36 -0200

    Google oAuth - Phase One

--- server/index.js
+++ server/index.js
@@ -2,10 +2,29 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// Authentication Lib
+// Authentication Process
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const { clientID, clientSecret } = require('./config/keys')
 
-passport.use(new GoogleStrategy())
+passport.use(
+  new GoogleStrategy({
+    clientID,
+    clientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken)
+    console.log('refreshToken', refreshToken)
+    console.log('profile', profile)
+  })
+)
 
+const googleAuthResponse = 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+
+  
+app.get('/auth/google', googleAuthResponse)
+app.get('/auth/google/callback', passport.authenticate('google'))
 app.listen(PORT)
