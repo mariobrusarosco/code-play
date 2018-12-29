9b271f8b16668260eaaff13d17ddfc881c513f80
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-29 12:11:58 -0200

    Heroku fix

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '731348302009-g5j4o550mgkn8hbhat4pr1uj19cdpsgt.apps.googleusercontent.com',
+    googleClientSecret: 'tkvBfEZBT_320rdjAtQv7Ljn',
+    mongoURI: 'mongodb://mario:walterwhite00@ds141623.mlab.com:41623/dev-code-play',
+    cookieKey: 'sdsdas*(8asdas8da90s8d023dasD_dfsaDsa)asd'
+  }
--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ if(process.env.NODE_ENV === 'production') {
   module.exports = require('./prod')
 }
 else {
+  console.log('requring: ', require('./dev'))
   module.exports = require('./dev')
 }
