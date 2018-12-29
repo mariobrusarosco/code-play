de3a2abd6b3e6e3a2c2b067113c0d539d07d84c4
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-29 12:11:58 -0200

    Heroku fix

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('users');
 
+console.log(keys)
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
