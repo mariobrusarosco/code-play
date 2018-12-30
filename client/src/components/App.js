6308b6afb3e33a42f087b92050355873dc00be64
Mario Brusarosco <mariobrusarosco@hotmail.com>
2018-12-30 09:50:23 -0200

    Setting Routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// Components
+import Header from '../components/Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNewer = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div> 
-      HI asdsad 
-    </div>
+		<BrowserRouter>
+			<div>
+				<Header />
+				<Route
+					path="/"
+					component={Landing}
+					exact
+				/>
+				<Route
+					path="/surveys"
+					component={Dashboard}
+					exact
+				/>
+				<Route
+					path="/surveys/new"
+					component={SurveyNewer}
+				/>
+			</div>
+		</BrowserRouter>
   )
 }
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+	render() {
+		return (
+			<h2>Heade!r</h2>
+		)
+	}
+}
+
+export default Header
