6c26f53962ac7c2c39675119f77b66f2d2da0b13
Mario Brusarosco <mariobrusarosco@gmail.com>
2019-01-20 17:16:35 -0200

    Preparing to deploy on heroku

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serving assets like main.css or main.js
+  // If this condition fits...code ends here!!
+  app.use(express.static('client/build'))
+
+  // If the server does not recognize a route... it's gonna serve index.html
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
